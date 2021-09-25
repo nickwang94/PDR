64968769eb28b7920b9204f5a2162122de31346f
Nick Wang <wenkun_wang@163.com>
2021-09-26 01:49:22 +0800

    【功能模块】：初始化项目
    【修改内容】：SpringBoot项目构建
    
    【影响范围】：

---
+++ sprintboot-pdr/src/test/java/com/nick/pdr/PdrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nick.pdr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
